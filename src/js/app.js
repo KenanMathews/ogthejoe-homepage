ada5f819a130b5bbda3da669020f06b94afa4915
KenanMathews <kenan.mathukutty@gmail.com>
2025-09-10 19:28:06 +0530

    Updated Homepage js file call

--- src/js/app.js
+++ src/js/app.js
@@ -213,6 +213,7 @@ class OgTheJoeHomepage {
             // Simple approach: try to fetch with no-cors mode
             const response = await fetch(service.healthEndpoint, {
                 method: 'HEAD',
+                mode: 'no-cors',
                 signal: controller.signal
             });
             
